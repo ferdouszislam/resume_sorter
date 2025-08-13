d7339f3eefbd3c9aa8b609642584d7c26aa620b9
Ferdous Islam <ferdous.islam@neural-semiconductor.com>
2025-08-13 10:50:37 +0600

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-from helpers import rag_db
+from helpers import rag_db, pdf_to_md
 
-# pdf_to_md.convert_pdfs_to_md(pdf_dir="./resumes", output_dir="./resumes_in_md")
+pdf_to_md.convert_pdfs_to_markdown(pdf_dir="./resumes", output_dir="./resumes_in_md")
 rag_db.drop_lancedb_table()
 rag_db.setup_lancedb()
